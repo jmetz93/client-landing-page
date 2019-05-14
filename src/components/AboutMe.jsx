463817a3c58fc78fdfdbd85aed1daaeb4dc4aa0f
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-05-14 13:23:38 -0700

    Applying image changes to deployed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,10 +6,10 @@ import { Container, Title, Bio, Subheader, Image } from '../styled-components/Ab
 // image
 import picture from '../assets/images/image001.jpg'
 
-const AboutMe = () => (
+const AboutMe = (props) => (
   <Container>
     <Title>Welcome To The Team!</Title>
-    <Image src={picture} style={{ height: '20%', width: '20%' }}></Image>
+    <Image src={picture} style={props.style}></Image>
     <Subheader>About me: </Subheader>
     
       <Bio>Ryan Mattingly has been a leader in the Final Expense Industry since 2016. He has previously held records for sales and Annual Premium at his last organization. To be successful in the Final Expense market, he believes that you need to be willing to give 100% every day, and as long as you follow our proven system, you have a high probability of succeeding. Ryan, a southern California native, grew up in Culver City, California and attended San Diego State University, where he received his bachelor’s degree in Business Management. He is very impressed with the earning potential and the training provided by management. He is looking forward to growing the Southern California territory for many years to come.</Bio>
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,36 +39,67 @@ const icons = {
 
 
 export default class App extends React.Component {
-  state = {}
+  state = {
+    style: {
+      height: '250px',
+      width: '175px',
+    },
+    loaded: false
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('resize', this.updateWindowDimensions);
+    this.updateWindowDimensions()
+  }
+
+  updateWindowDimensions = () => {
+    console.log('update')
+    if(window.innerWidth < 640) {
+      this.setState({
+        style: {
+          height: '135px',
+          width: '100px'
+        },
+        loaded: true
+      })
+    } else {
+      this.setState({
+        loaded: true
+      })
+    } 
+  }
 
   render() {
+    const { style, loaded } = this.state
     return (
       <div>
-        <Header>
-          <Title>FINAL EXPENSE SALES TRAINING!</Title>
-        </Header>
-        <Container>
-          <AboutMe />
-          <TrainingVids />
-          <RateCalc />
-          <OrderLeads />
-          <Downloads />
-          <Contact />
-        </Container>
-        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-            <div style={phantom} />
-            <div style={style}>
-                <div>Website built and designed by Jacob Metzinger{"\n"}</div>
-                <div>For business inquiries:</div>
-                <div style={{marginBottom: '1%'}}>
-                  <a href={"mailto:" + 'jacob.adam.metzinger@gmail.com'}>jacob.adam.metzinger@gmail.com</a>
-                  <div style={{ marginTop: '.3%'}}>(424)216-2291</div>
-                </div>
-                <div style={icons}> 
-                  <SocialIcon url="https://www.linkedin.com/in/jacob-metzinger-a2b8a7142/" />
-                </div>
-            </div>
-        </div>
+        {loaded && <div>
+          <Header>
+            <Title>FINAL EXPENSE SALES TRAINING!</Title>
+          </Header>
+          <Container>
+            <AboutMe style={style}/>
+            <TrainingVids />
+            <RateCalc />
+            <OrderLeads />
+            <Downloads />
+            <Contact />
+          </Container>
+          <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+              <div style={phantom} />
+              <div style={style}>
+                  <div>Website built and designed by Jacob Metzinger{"\n"}</div>
+                  <div>For business inquiries:</div>
+                  <div style={{marginBottom: '1%'}}>
+                    <a href={"mailto:" + 'jacob.adam.metzinger@gmail.com'}>jacob.adam.metzinger@gmail.com</a>
+                    <div style={{ marginTop: '.3%'}}>(424)216-2291</div>
+                  </div>
+                  <div style={icons}> 
+                    <SocialIcon url="https://www.linkedin.com/in/jacob-metzinger-a2b8a7142/" />
+                  </div>
+              </div>
+          </div>
+        </div>}
       </div>
     )
   }
